87d60ca0c40383863821d1c6184184da2e7d6798
曾星 <zengxing@udesk.cn>
2022-06-16 17:28:04 +0800

    feature(iAccount): start

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,48 @@
 <script setup lang="ts">
-import router from '@/router'
-import { ElButton } from 'element-plus'
+import { useRouter } from 'vue-router'
+import {
+    ElContainer,
+    ElHeader,
+    ElAside,
+    ElMain,
+    ElMenu,
+    ElMenuItem,
+    ElIcon,
+} from 'element-plus'
+import { Document, Menu as IconMenu } from '@element-plus/icons-vue'
 
-const jump = (path: string) => {
-    router.push(path)
+const router = useRouter()
+const handleSelect = (key: string, keyPath: string[]) => {
+    console.log(key, keyPath)
+    router.push(key)
 }
 </script>
 
 <template>
-    <el-button type="primary" @click="() => jump('/games/saolei')">
-        扫雷
-    </el-button>
-    <el-button type="primary" @click="() => jump('/games/zhizhu')">
-        蜘蛛纸牌
-    </el-button>
+    <el-container>
+        <el-header>
+            <h5>Armito's Account</h5>
+        </el-header>
+        <el-container>
+            <el-aside width="200px">
+                <el-menu
+                    default-active="2"
+                    class="el-menu-vertical-demo"
+                    @select="handleSelect"
+                >
+                    <el-menu-item index="/list">
+                        <el-icon><icon-menu /></el-icon>
+                        <span>我的列表</span>
+                    </el-menu-item>
+                    <el-menu-item index="/detail">
+                        <el-icon><document /></el-icon>
+                        <span>我的详情</span>
+                    </el-menu-item>
+                </el-menu>
+            </el-aside>
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
---
+++ src/pages/index/List/index.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import {
+    ElCard,
+    ElButton,
+    ElDescriptions,
+    ElDescriptionsItem,
+} from 'element-plus'
+
+const list = [
+    {
+        id: '1',
+        appName: 'weixin',
+        account: 'sadijdj923ij',
+        password: '943u9i43',
+    },
+    {
+        id: '2',
+        appName: 'qq',
+        account: 'sadsad',
+        password: '34556654t45',
+    },
+    {
+        id: '3',
+        appName: 'weibo',
+        account: 'plplplp3',
+        password: '9ifd09r09fj3',
+    },
+]
+</script>
+
+<template>
+    <div>
+        <el-card v-for="item in list" :key="item.id" class="box-card">
+            <div class="text item">
+                <el-descriptions :title="item.appName" :column="2">
+                    <el-descriptions-item label="账号">
+                        {{ item.account }}
+                    </el-descriptions-item>
+                    <el-descriptions-item label="密码">
+                        {{ item.password }}
+                    </el-descriptions-item>
+                </el-descriptions>
+            </div>
+        </el-card>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.box-card {
+    margin-bottom: 20px;
+}
+</style>
